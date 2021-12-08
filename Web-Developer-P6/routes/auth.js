766fbc9eb392d7544eff9973be2d4e22c58f530e
Ibrahim Coulibaly <ibrahim.abe.coulibaly@gmail.com>
2021-12-08 14:40:18 +0100

    ajout possibilité connexion

--- Web-Developer-P6/routes/auth.js
+++ Web-Developer-P6/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/User');
 
-router.post('signup', userCtrl.signup);
-router.post('login', userCtrl.login);
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
 module.exports = router;
